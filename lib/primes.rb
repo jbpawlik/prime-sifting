ebcfe007e6920ae3a658d12534121725b8c2bc41
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-15 11:30:56 -0700

    add spec to remove multiples of two; spec passing
    Co-authored-by: mireie
    <mireie@users.noreply.github.com>

--- lib/primes.rb
+++ lib/primes.rb
@@ -8,6 +8,11 @@ class PrimeFinder
   def find_primes
     num_array=[]
     (2..@number).each {|i| num_array.push(i)}
+    num_array.each_with_index do |element, index|
+      if element%2 === 0 && element !=2
+        num_array.delete_at(index)
+      end
+    end
     return num_array
   end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -2,8 +2,12 @@ require 'rspec'
 require 'primes'
 
 describe('#find_primes') do
-  it('should return primes up to two') do
+  it('returns primes up to two') do
     number = PrimeFinder.new(2)
     expect(number.find_primes).to(eq([2]))
+  end
+  it('removes all multiples of 2 other than 2') do
+    number = PrimeFinder.new(10)
+    expect(number.find_primes).to(eq([2,3,5,7,9]))
   end
 end
